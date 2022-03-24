cf94bd5e677c1114dba55b933e811ccdc8870fcd
David Rose <david.sr.rose@protonmail.com>
2022-03-24 11:28:14 -0600

    updated to only advance a sum if it is a goal

--- advance_game_board.R
+++ advance_game_board.R
@@ -7,11 +7,16 @@
 # notes on improvements and additions to function
 
 #-------------------------------------------------------------------------------
-advance_game_board <- function(sums_chosen, game_board_df) {
+advance_game_board <- function(sums_chosen,
+                               game_board_df,
+                               goals) {
     for (i in 1:2) {
-        # only advance for the chosen sum if it is not captured!
-        if (game_board_df$current_progress[sums_chosen[i] - 1] !=
-            game_board_df$rolls_required_to_capture) {
+        # only advance for the chosen sum if it is not captured!,
+        # and only advance if the sum chosen is a goal
+        if ((game_board_df$current_progress[sums_chosen[i] - 1] !=
+            game_board_df$rolls_required_to_capture) &&
+            (sums_chosen[i] %in% goals){
+                
             game_board_df$current_progress[sums_chosen[i] - 1] <-
                 game_board_df$current_progress[sums_chosen[i] - 1] + 1
         }
